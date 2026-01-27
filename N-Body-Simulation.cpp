0936fb7250565e9782d18b7e64b0d7d3082c1433
Andy Xu <xuyuchen100@gmail.com>
2026-01-26 22:43:26 -0500

    Add project files.

---
+++ N-Body-Simulation.cpp
@@ -0,0 +1,12 @@
+﻿// N-Body-Simulation.cpp : Defines the entry point for the application.
+//
+
+#include "N-Body-Simulation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ N-Body-Simulation.h
@@ -0,0 +1,8 @@
+﻿// N-Body-Simulation.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
